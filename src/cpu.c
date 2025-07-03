66f1cfe055c1385d1ef44a32a5197dc626761193
Piotr Laskowski <szczota876@gmail.com>
2025-07-03 23:48:38 +0000

    added some opcodes and tried building some functions (failed miserably)

--- src/cpu.c
+++ src/cpu.c
@@ -7,36 +7,54 @@ cpu_t init_cpu(uint8_t rom[]){
    fetch(&cpu);
 }
 
+void inc16BitRegister(uint16_t *reg){
+   *reg++;
+}
+
+void dec16BitRegister(uint16_t *reg){
+   *reg--;
+}
+
+void LD_into16BitRegister(char *reg,cpu_t *cpu ,uint16_t value){
+   if(strcmp(reg, "BC")){
+      cpu->reg.BC = value;
+   }
+}
 
 void readOpcode(cpu_t* cpu, uint8_t opcode){
       switch (opcode)
       {
       //todo
-      case 0x00: printf("nop"); break;
-      case 0x01: printf("LD BC, %x",read16(cpu)); break;
-      case 0x02: printf("nop"); break;
-      case 0x03: printf("nop"); break;
-      case 0x04: printf("nop"); break;
-      case 0x05: printf("nop"); break;
-      case 0x06: printf("nop"); break;
-      case 0x07: printf("nop"); break;
-      case 0x08: printf("nop"); break;
-      case 0x09: printf("nop"); break;
-      case 0x0A: printf("nop"); break;
-      case 0x0B: printf("nop"); break;
-      case 0x0E: printf("nop"); break;
-      case 0x0F: printf("nop"); break;
+      case 0x00: printf("nop");  break;;
+      case 0x01: printf("LD BC, n16"); LD_into16BitRegister("BC",cpu, read16(cpu)); printf("%x",cpu->reg.BC); break;
+      case 0x02: printf("LD [BC], A"); break;
+      case 0x03: printf("INC BC"); break;
+      case 0x04: printf("INC B"); break;
+      case 0x05: printf("DEC B"); break;
+      case 0x06: printf("LD B, n8"); break;
+      case 0x07: printf("RLCA"); break;
+      case 0x08: printf("LD [a16], SP"); break;
+      case 0x09: printf("ADD HL BC"); break;
+      case 0x0A: printf("LD A [BC]"); break;
+      case 0x0B: printf("DEC BC"); break;
+      case 0x0C: printf("INC C"); break;
+      case 0x0D: printf("DEC C"); break;
+      case 0x0E: printf("LD, C, n8"); break;
+      case 0x0F: printf("RRCA"); break;
       default:
+         printf("undefined func");
          break;
       }
+      printf("\n");
+      cpu->reg.PC++;
 }
 
 void fetch(cpu_t *cpu){
    uint16_t pc = cpu->reg.PC;
-   if(pc >= 0x0000 && pc <= 0x3FFF){
-      readOpcode(cpu,cpu->mem.ram[cpu->reg.PC]);
-      readOpcode(cpu,cpu->mem.ram[cpu->reg.PC]);
-      readOpcode(cpu,cpu->mem.ram[cpu->reg.PC]);
-   }
+   readOpcode(cpu,cpu->mem.ram[cpu->reg.PC]);
+   readOpcode(cpu,cpu->mem.ram[cpu->reg.PC]);
+   readOpcode(cpu,cpu->mem.ram[cpu->reg.PC]);
+   readOpcode(cpu,cpu->mem.ram[cpu->reg.PC]);
+   readOpcode(cpu,cpu->mem.ram[cpu->reg.PC]);
    //remove this shit
 }
--- src/memory.c
+++ src/memory.c
@@ -1,15 +1,16 @@
 #include "memory.h"
 #include "cpu.h"
 
-//add read8,write8 write16
 
-uint16_t read16(cpu_t* cpu){
+
+uint16_t read16(cpu_t *cpu){
    uint8_t byte1 = cpu->mem.ram[cpu->reg.PC++];
    uint8_t byte2 = cpu->mem.ram[cpu->reg.PC++];
    uint16_t value = (byte2 << 8) | byte1;
    return value;
 }
 
+
 int loadRomToBank(cpu_t *cpu, uint8_t rom[]){
-    memcpy(cpu->mem.ram, rom, ROM_BANK00_SIZE);
+    memcpy(cpu->mem.ram, rom, ROM_BANK00_SIZE + ROM_BANK01_SIZE);
 }
